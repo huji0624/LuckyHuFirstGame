735e16b08ae548f284ba6a338ba636b2d84e71c0
Luckyhu <huji0624@gmail.com>
2014-04-09 00:34:32 +0800

    change proj config.
    Make android use svglevelreader.

--- LuckyHuFirstGame-desktop/src/com/luckyhu/game/Main.java
+++ LuckyHuFirstGame-desktop/src/com/luckyhu/game/Main.java
@@ -3,8 +3,9 @@ package com.luckyhu.game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.luckyhu.game.bal.LHBallGame;
+import com.luckyhu.game.framework.game.util.LHADable;
 
-public class Main {
+public class Main implements LHADable{
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "LuckyHuFirstGame";
@@ -12,6 +13,20 @@ public class Main {
 		cfg.width = 320;
 		cfg.height = 480;
 		
+		LHBallGame.adImp = new Main();
+		
 		new LwjglApplication(new LHBallGame(), cfg);
+	}
+
+	@Override
+	public void showAd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hideAd() {
+		// TODO Auto-generated method stub
+		
 	}
 }
